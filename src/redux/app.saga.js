5768b0e2e6525227639c1e51c822660a620bb5ef
farciomernandes <farciomernandes@gmail.com>
2021-04-27 15:09:58 -0300

    add comments

--- src/redux/app.saga.js
+++ src/redux/app.saga.js
@@ -3,9 +3,14 @@ import { takeLatest, delay, put } from 'redux-saga/effects';
 export function* onIncrement() {
   yield console.log('I am incremented');
   yield delay(3000);
+  //put = awaits the execution of all calls to apply the final status of all calls simultaneously.
   yield put({ type: 'INCREMENT_FROM_SAGA' });
 }
 
 export function* incrementSaga() {
+  //takeLates = cancels all calls to the function and makes only the last one.
   yield takeLatest('INCREMENT', onIncrement);
 }
+
+//take = if if the function is repeated, and there is a delay, it may not execute every time.
+//takeEvery = creates "subsagas" that perform all the functions called even if they have a delay.
